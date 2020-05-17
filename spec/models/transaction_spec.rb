9b383aef12e1f8fe95bbf2edaaf55c93fe92d3f7
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-05-17 11:41:22 -0600

    Add validations for transactions

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'releationships' do
+    it { should belong_to :invoice}
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :credit_card_number }
+    it { should validate_presence_of :result }
+    it { should define_enum_for(:result).with_values([:success, :failed])}
+
+  end
+  
+  describe 'instance methods' do
+    
+  end
 end
