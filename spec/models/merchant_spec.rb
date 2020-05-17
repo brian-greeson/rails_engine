95ef28125022399b47cf51b56a9b9fcf612ad982
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-05-17 10:39:36 -0600

    Add validations and relationship to mechants

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'releationships' do
+
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :name}
+    it { should have_many :items}
+  end
+  
+  describe 'instance methods' do
+    
+  end
 end
