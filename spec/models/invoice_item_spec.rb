7eb2fcfdbebcc6c6a05ee1abbabdfe882dcf371f
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-05-17 11:33:56 -0600

    Add validations to invoice_items

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe InvoiceItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'releationships' do
+    it { should belong_to :invoice}
+    it { should belong_to :item}
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :quantity}
+    it { should validate_presence_of :unit_price}
+  end
+  
+  describe 'instance methods' do
+    
+  end
 end
