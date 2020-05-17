4abf8232bd29c83fcc15490be9437fadae135a5d
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-05-17 11:00:46 -0600

    Add validations for invoices

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,8 @@
 class Invoice < ApplicationRecord
   belongs_to :customer
   belongs_to :merchant
+
+  validates_presence_of :status
+
+  enum status: [:pending, :fulfilled, :shipped, :canceled]
 end
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Invoice, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'releationships' do
+    it { should belong_to :merchant }
+    it { should belong_to :customer }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :status }
+    it { should define_enum_for(:status).with_values([:pending, :fulfilled, :shipped, :canceled])}
+  end
+  
+  describe 'instance methods' do
+    
+  end
 end
