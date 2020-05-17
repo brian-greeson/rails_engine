c05c83dae146147788ec6a85484daf092b08c7f9
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-05-17 10:42:44 -0600

    Move test condition to correct describe

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -2,12 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
   describe 'releationships' do
-
+    it { should have_many :items}
   end
 
   describe 'validations' do
     it { should validate_presence_of :name}
-    it { should have_many :items}
   end
   
   describe 'instance methods' do
