507cfa4ed7c82325dd1e23d204dd0592407c9354
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-05-17 11:10:12 -0600

    Add validations to invoices

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,8 +1,12 @@
 class Invoice < ApplicationRecord
   belongs_to :customer
   belongs_to :merchant
+  has_many :transactions
+  has_many :invoice_items
+  has_many :items, through: :invoice_items
 
-  validates_presence_of :status
+
+  validates_presence_of :status, :customer_id, :merchant_id
 
   enum status: [:pending, :fulfilled, :shipped, :canceled]
 end
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -4,10 +4,15 @@ RSpec.describe Invoice, type: :model do
   describe 'releationships' do
     it { should belong_to :merchant }
     it { should belong_to :customer }
+    it { should have_many :invoice_items }
+    it { should have_many(:items).through(:invoice_items)}
+    it { should have_many :transactions}
   end
 
   describe 'validations' do
     it { should validate_presence_of :status }
+    it { should validate_presence_of :merchant_id }
+    it { should validate_presence_of :customer_id }
     it { should define_enum_for(:status).with_values([:pending, :fulfilled, :shipped, :canceled])}
   end
   
