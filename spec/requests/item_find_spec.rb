9a5ac5955eda79331310f4aed2a908529fe4fa28
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-05-19 16:21:29 -0600

    Add item find test

---
+++ spec/requests/item_find_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+describe 'Items Finder API' do
+  describe 'Single Results Finder' do
+    after(:all) do
+      Item.destroy_all
+      Merchant.destroy_all
+    end
+
+    it 'Can Find an item by name' do
+      merchant1 = create(:merchant)
+      item1 = merchant1.items.create(attributes_for(:item))
+      item2 = merchant1.items.create(attributes_for(:item))
+      item3 = merchant1.items.create(attributes_for(:item))
+
+      get "/api/v1/items/find?name=#{item1.name}"
+      expect(response).to be_successful
+      item = JSON.parse(response.body)
+      
+      expect(item["data"]["type"]).to eq("item")
+      expect(item["data"]["id"]).to eq(item1.id.to_s)
+      expect(item["data"]["attributes"]["name"]).to eq(item1.name)
+      expect(item["data"]["attributes"]["description"]).to eq(item1.description)
+      expect(item["data"]["attributes"]["unit_price"]).to eq(item1.unit_price.to_f)
+      expect(item["data"]["attributes"]["merchant_id"]).to eq(item1.merchant_id)
+    end
+
+    # it 'Can find an item by partial name' do
+    #   merchant1 = create(:merchant)
+    #   item1 = merchant1.items.create(attributes_for(:item))
+    #   item2 = merchant1.items.create(attributes_for(:item))
+    #   item3 = merchant1.items.create(attributes_for(:item))
+
+    #   get "/api/v1/items/find?name=#{item1.name}"
+    #   expect(response).to be_successful
+    #   item = JSON.parse(response.body)
+    # end
+  end
+end
