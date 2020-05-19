463c42a2acd9486b522d931a8b9c1601fdcf4ced
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-05-18 22:09:08 -0600

    Fix spelling in item merchant route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :items do
-        get "/merchants", to: "item_merchants#index"
+        get "/merchant", to: "item_merchant#index"
       end
       
       resources :merchants do
