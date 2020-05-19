6940d701cb648ed15bfba8abd4b144a2502f75a4
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-05-18 20:15:40 -0600

    Add merchant items endpoint test

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :items, only: [:index, :show, :destroy, :create, :update]
-      resources :merchants, only: [:index, :show, :destroy, :create, :update]
+      resources :items do
+        get "/merchants", to: "item_merchants#index"
+      end
+      
+      resources :merchants do
+        get "/items", to: "merchant_items#index"
+      end
+      
     end
   end
 end
