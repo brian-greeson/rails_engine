3965e980554b0a4951224dcb108934120d46a020
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-05-19 18:34:02 -0600

    Add find_matches to models

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,12 +4,16 @@ class Item < ApplicationRecord
   validates_presence_of :name, :description, :unit_price
 
   def self.find_match(params)
+    find_matches(params).first
+  end
+
+  def self.find_matches(params)
     result = nil
     params.each do |column, search|
       result = where("#{column} ILIKE ?", "%#{search}%")
       break if !result.empty?
     end
-    result.first
+    result
   end
 end
 
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -5,11 +5,15 @@ class Merchant < ApplicationRecord
   validates_presence_of :name
 
   def self.find_match(params)
+    find_matches(params).first
+  end
+
+  def self.find_matches(params)
     result = nil
     params.each do |column, search|
       result = where("#{column} ILIKE ?", "%#{search}%")
       break if !result.empty?
     end
-    result.first
+    result
   end
 end
